467c1f6ae1486a54f40ff233aa5d55a70c3e46ff
hikdogru <hikdogru@gmail.com>
2021-08-13 23:20:25 +0300

    Create the SearchResults view

--- BookPriceComparasion.WebUI/Controllers/HomeController.cs
+++ BookPriceComparasion.WebUI/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -16,9 +17,16 @@ namespace BookPriceComparasion.WebUI.Controllers
     public class HomeController : Controller
     {
         private static ChromeOptions _chromeOptions;
-        private string[] _urlList = {"https://www.amazon.com.tr/s?k=", "https://www.bkmkitap.com/arama?q=" };
-        private string _query = "Otomatik Portakal";
+        private static Thread _threadAmazon;
+        private static Thread _threadKidega;
+        private static Thread _threadBkm;
+        private static WebDriverWait _wait;
+
+
         private readonly ILogger<HomeController> _logger;
+        private string[] _urlList = { "https://www.amazon.com.tr/s?k=", "https://www.bkmkitap.com/arama?q=", "https://kidega.com/arama/", "https://www.idefix.com/search/?Q=" };
+        private string _query = "Otomatik Portakal";
+
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -27,34 +35,129 @@ namespace BookPriceComparasion.WebUI.Controllers
 
         public IActionResult Index()
         {
-            _chromeOptions = new ChromeOptions();
-            //var user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36";
-            _chromeOptions.AddArgument("--window-size=1920,1080");
-            _chromeOptions.AddArgument("--start-maximized");
-            //_chromeOptions.AddArgument("--headless");
-            using IWebDriver driver = new ChromeDriver(_chromeOptions);
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-            foreach (var url in _urlList)
-            {
-                driver.Navigate().GoToUrl(url + _query);
-                //driver.FindElement(By.Name("q")).SendKeys("cheese" + Keys.Enter);
-                if (url.Contains("amazon"))
-                {
-                    wait.Until(webDriver => webDriver.FindElement(By.CssSelector("img[class='s-image']")).Displayed);
-                    IWebElement firstResult = driver.FindElement(By.CssSelector("img[class='s-image']"));
-                    TempData["AmazonBookImage"] += firstResult.GetAttribute("src");
-                }
-                else
-                {
-                    wait.Until(webDriver => webDriver.FindElement(By.CssSelector("img[class*='stImage']")).Displayed);
-                    IWebElement firstResult = driver.FindElement(By.CssSelector("img[class*='stImage']"));
-                    TempData["BkmBookImage"] += firstResult.GetAttribute("src");
-                }
-                
-            }
-            
+
+            //Stopwatch stopwatch = Stopwatch.StartNew();
+
+
+
+            //foreach (var url in _urlList)
+            //{
+            //    //driver.FindElement(By.Name("q")).SendKeys("cheese" + Keys.Enter);
+            //    if (url.Contains("amazon"))
+            //    {
+            //        _threadAmazon = new Thread(() =>
+            //            FindElementAmazon(By.CssSelector("img[class='s-image']"), url, "Amazon"));
+            //        _threadAmazon.Start();
+
+
+
+            //    }
+            //    else if (url.Contains("kidega"))
+            //    {
+            //        _threadKidega = new Thread(() =>
+            //            FindElementKidega(By.CssSelector("img[class='lazyload activeted loadedImg']"), url, "Kidega"));
+            //        _threadKidega.Start();
+
+            //    }
+            //    else
+            //    {
+            //        _threadBkm = new Thread(() => FindElementBkm(By.CssSelector("img[class*='stImage']"), url, "Bkm"));
+            //        _threadBkm.Start();
+
+            //    }
+            //}
+
+
+
+            //Debug.WriteLine("**************************************");
+            //Debug.WriteLine($"Elapsed time {stopwatch.ElapsedMilliseconds} ");
+            //stopwatch.Stop();
 
             return View("Index", "Hi");
+        }
+
+        [HttpPost]
+        public IActionResult Search(string query)
+        {
+            if (!String.IsNullOrEmpty(query))
+            {
+                _query = query;
+            }
+
+            GetChromeOptions();
+            _threadAmazon = new Thread(() =>
+                FindElementAmazon(new[] { By.CssSelector("img[class='s-image']"), By.CssSelector("img[class=' lazyloaded']") }, new[] { _urlList[0], _urlList[3] },
+                    new[] { "Amazon", "Idefix" }));
+            _threadAmazon.Start();
+
+
+
+            _threadKidega = new Thread(() =>
+                FindElementKidega(By.CssSelector("img[class='lazyload activeted loadedImg']"), _urlList[2], "Kidega"));
+            _threadKidega.Start();
+
+            _threadBkm = new Thread(() => FindElementBkm(By.CssSelector("img[class*='stImage']"), _urlList[1], "Bkm"));
+            _threadBkm.Start();
+
+            _threadAmazon.Join();
+            _threadBkm.Join();
+            _threadKidega.Join();
+
+
+
+            return View("SearchResults");
+        }
+
+        public void GetChromeOptions()
+        {
+            _chromeOptions = new ChromeOptions();
+            var userAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36";
+            _chromeOptions.AddArgument("--window-size=1920,1080");
+            _chromeOptions.AddArgument("--start-maximized");
+            _chromeOptions.AddArgument($"user_agent={userAgent}");
+            _chromeOptions.AddArgument("--ignore-certificate-errors");
+            _chromeOptions.AddArgument("no-sandbox");
+            _chromeOptions.AddArgument("--disable-gpu");
+            _chromeOptions.AddArgument("--headless");
+        }
+
+        public void FindElementAmazon(By[] by, string[] url, string[] websiteName)
+        {
+            for (int i = 0; i < by.Length; i++)
+            {
+                using var driver = new ChromeDriver(_chromeOptions);
+                driver.Navigate().GoToUrl(url[i] + _query);
+                _wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+                _wait.Until(webDriver => webDriver.FindElement(by[i]).Displayed);
+                IWebElement firstResult = driver.FindElement(by[i]);
+
+                TempData[$"{websiteName[i]}BookImage"] = firstResult.GetAttribute("src");
+            }
+
+
+        }
+
+        public void FindElementBkm(By by, string url, string websiteName)
+        {
+            using var driver = new ChromeDriver(_chromeOptions);
+            driver.Navigate().GoToUrl(url + _query);
+            _wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            _wait.Until(webDriver => webDriver.FindElement(by).Displayed);
+            IWebElement firstResult = driver.FindElement(by);
+            TempData["BkmBookImage"] = firstResult.GetAttribute("src");
+
+
+
+        }
+
+        public void FindElementKidega(By by, string url, string websiteName)
+        {
+            using var driver = new ChromeDriver(_chromeOptions);
+            driver.Navigate().GoToUrl(url + _query);
+            _wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            _wait.Until(webDriver => webDriver.FindElement(by).Displayed);
+            IWebElement firstResult = driver.FindElement(by);
+            TempData["KidegaBookImage"] = firstResult.GetAttribute("src");
         }
 
         public IActionResult Privacy()
